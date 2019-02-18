417f8d618410d841f5c78f88cfab3f02f6cf135b
GermanAizek <GermanAizek@yandex.ru>
2019-02-18 14:16:38 +0300

    [ADD] Added getinfo, write out result, rename vars

--- calc.py
+++ calc.py
@@ -33,12 +33,18 @@ def getSkuItemsAppend(listAppend, listItems):
 	for item in listItems:
 		listAppend.append(item.sku)
 
-#def getInfoBySku(sku):
-	#
+def getInfoBySku(sku):
+	for item in listItems:
+		for item in listItemsSite:
+			if sku == item.sku:
+				break
+
+	return item.name, item.price
+
  
 if __name__ == "__main__":
 	fileReadAppend(config['DEFAULT']['NameFile'] + '.' + config['DEFAULT']['FormatPrice'], listItems)
-	fileReadAppend(config['DEFAULT']['NameFileOld'] + '.' + config['DEFAULT']['FormatPrice'], listItemsSite)
+	fileReadAppend(config['DEFAULT']['NameFileSecond'] + '.' + config['DEFAULT']['FormatPrice'], listItemsSite)
 
 	getSkuItemsAppend(listPrice, listItems)
 	getSkuItemsAppend(listPriceSite, listItemsSite)
@@ -51,7 +57,32 @@ if __name__ == "__main__":
 
 	listUnchanged = list(set(listPrice) & set(listPriceSite))
 
-	print('Unchanged: ' + str(listUnchanged))
-	print('Exist in ' + config['DEFAULT']['NameFile'] + '.' + config['DEFAULT']['FormatPrice'] + ': ' + str(listValid))
-	print('Exist in ' + config['DEFAULT']['NameFileOld'] + '.' + config['DEFAULT']['FormatPrice'] + ': ' + str(listNotValid))
+	# print('Unchanged: ' + str(listUnchanged) + '\n')
 	
+	# print('Exist in ' + config['DEFAULT']['NameFile'] + '.' + config['DEFAULT']['FormatPrice'] + ': ' + str(listValid) + '\n')
+	# print(" SKU      Name      Price")
+	# for sku in listValid:
+	# 	print(" " + sku + " " + getInfoBySku(sku)[0] + " " + getInfoBySku(sku)[1])
+	
+	# print('\nExist in ' + config['DEFAULT']['NameFileSecond'] + '.' + config['DEFAULT']['FormatPrice'] + ': ' + str(listNotValid) + '\n')
+	# print(" SKU      Name      Price")
+	# for sku in listNotValid:
+	# 	print(" " + sku + " " + getInfoBySku(sku)[0] + " " + getInfoBySku(sku)[1])
+
+	with open(config['DEFAULT']['NameFileOut'] + '.' + config['DEFAULT']['FormatPrice'], 'w', newline='') as csvfile:
+		csvfile.write('Exist in ' + config['DEFAULT']['NameFile'] + '.' + config['DEFAULT']['FormatPrice'] + '\n\n')
+
+		fieldnames = ['_SKU_', '_NAME_', '_PRICE_']
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=config['CSV']['DelimiterCSV'])
+
+		writer.writeheader()
+
+		for sku in listValid:
+			writer.writerow({'_SKU_': sku, '_PRICE_': getInfoBySku(sku)[1], '_NAME_': getInfoBySku(sku)[0]})
+
+		csvfile.write('\nExist in ' + config['DEFAULT']['NameFileSecond'] + '.' + config['DEFAULT']['FormatPrice'] + '\n\n')
+
+		writer.writeheader()
+		
+		for sku in listNotValid:
+			writer.writerow({'_SKU_': sku, '_PRICE_': getInfoBySku(sku)[1], '_NAME_': getInfoBySku(sku)[0]})
